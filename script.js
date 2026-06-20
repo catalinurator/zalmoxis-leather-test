a535372dae9c136a00518391442faa99aa8114eb
catalinurator <catalinurator@gmail.com>
2026-06-20 08:23:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ function toggleMenu(){
 document.getElementById("sidebar").classList.toggle("open");
 document.getElementById("overlay").classList.toggle("active");
 }
+function revealOnScroll(){
+const elements = document.querySelectorAll(".card, section, .header");
+
+elements.forEach(el=>{
+const top = el.getBoundingClientRect().top;
+const windowHeight = window.innerHeight;
+
+if(top < windowHeight - 100){
+el.classList.add("show");
+}
+});
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
